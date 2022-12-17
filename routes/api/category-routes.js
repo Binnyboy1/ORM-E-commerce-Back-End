1c1f0564e82e4482886e4b6656d04492976b1e3b
Binny <ibinstock@outlook.com>
2022-12-17 09:29:18 -0800

    finished get('/:id') for /api/categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,18 +6,33 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   try {
-    const categoryData = Category.findAll();
+    const categoryData = Category.findAll({
+      // be sure to include its associated Products
+      include: [{ model: Product }]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products !!!
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categiry found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
